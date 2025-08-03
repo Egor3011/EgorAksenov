d0add6adc0266ec0dfb6dec2ad7aa7d122118ef7
Egor Aksenov <anonaso3011@gmail.com>
2025-08-03 23:06:13 +0300

    fix: url form

--- frontend/src/components/Form.vue
+++ frontend/src/components/Form.vue
@@ -51,7 +51,7 @@ const onClickSend = () => {
     }
     else {
         confirm("Отправить данные?")
-        axios.post("http://localhost/api/form", {
+        axios.post("http://176.123.166.173/api/form", {
             name: firstname,
             sname: sname,
             number: number,
