4272b467336dcbdcd85214fd6cf8f367a3a14ab7
Egor Aksenov <anonaso3011@gmail.com>
2025-08-03 23:48:32 +0300

    fix: ports cfg

--- frontend/src/components/Form.vue
+++ frontend/src/components/Form.vue
@@ -51,7 +51,7 @@ const onClickSend = () => {
     }
     else {
         confirm("Отправить данные?")
-        axios.post('/api/form', {
+        axios.post('http://localhost/api/form', {
             name: firstname,
             sname: sname,
             number: number,
