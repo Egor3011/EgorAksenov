a5bdd13e79572dbb57504bbfeba5dde3eb3fd397
Egor Aksenov <anonaso3011@gmail.com>
2025-08-08 23:51:37 +0300

    test

--- TGbot/main.py
+++ TGbot/main.py
@@ -7,6 +7,7 @@ bot = telebot.TeleBot(config.BOT_TOKEN)
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, "Привет")
-    print(message)
+    print({message.chat.id, message.chat.username})
+
 
 bot.polling(none_stop=True)
