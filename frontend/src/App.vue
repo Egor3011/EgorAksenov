1fbc0252f989fd3a5eafc2fed779789dcd8d1c37
Egor Aksenov <anonaso3011@gmail.com>
2025-08-11 22:56:05 +0300

    test gsap

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,14 +2,22 @@
 import { RouterLink, RouterView } from 'vue-router'
 import Footer from './components/Footer.vue';
 import headerMain from './components/headerMain.vue';
+import { gsap } from "gsap";
+    
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".headerTestGSAP", {
+  y: 300,
+})
 </script>
 
 <template>
   <header>
 
     <div class="wrapper">
-      <headerMain/>
+      <headerMain class="headerTestGSAP"/>
     </div>
   </header>
   <RouterView />
